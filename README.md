ee1eaec489049b2852b208846e9f3df72c8f45e0
Cauan Massino <cauanmassino55@gmail.com>
2024-09-16 10:48:42 -0300

    primeiro commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Bikcraft
+
+
+## Bikcraft
---
+++ js/script.js
@@ -0,0 +1,66 @@
+//ativar links do menu
+const links = document.querySelectorAll(".header-menu a");
+
+function AtivarLink(link) {
+  const url = location.href;
+  const href = link.href;
+  if (url.includes(href)) {
+    link.classList.add("ativo");
+  }
+}
+links.forEach(AtivarLink);
+
+// ativar items do orçamento
+const parametros = new URLSearchParams(location.search);
+
+function ativarProduto(parametro) {
+  const elemento = document.getElementById(parametro);
+  if (elemento) {
+    elemento.checked = true;
+  }
+}
+parametros.forEach(ativarProduto);
+
+//perguntas frequentes
+const perguntas = document.querySelectorAll(".perguntas button");
+
+function ativarPergunta(event) {
+  const pergunta = event.currentTarget;
+  const controls = pergunta.getAttribute("aria-controls");
+  const resposta = document.getElementById(controls);
+
+  resposta.classList.toggle("ativa");
+  const ativa = resposta.classList.contains("ativa");
+
+  pergunta.setAttribute("aria-expanded", ativa);
+}
+
+function eventosPerguntas(pergunta) {
+  pergunta.addEventListener("click", ativarPergunta);
+}
+
+perguntas.forEach(eventosPerguntas);
+
+// galeria de bicicletas
+const galeria = document.querySelectorAll(".bicicleta-imagens img");
+const galeiraContainer = document.querySelector(".bicicleta-imagens");
+
+function trocarImagem(event) {
+  const img = event.currentTarget;
+  const media = matchMedia("(min-width:1000px)").matches;
+
+  if (media) {
+    galeiraContainer.prepend(img);
+  }
+}
+
+function eventosGaleria(img) {
+  img.addEventListener("click", trocarImagem);
+}
+
+galeria.forEach(eventosGaleria);
+
+// animação
+if (window.SimpleAnime) {
+  new SimpleAnime();
+}
